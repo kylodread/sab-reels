3df8843e67c102dec7ec6f39742447836658d50c
kylodread <joshuanichols2204@gmail.com>
2024-10-29 18:27:33 +0200

    removes 2

--- src/app/(dashboard)/navbar.tsx
+++ src/app/(dashboard)/navbar.tsx
@@ -4,7 +4,7 @@ export const Navbar = () => {
   return (
     <nav className="w-full flex items-center p-4 h-[68px]">
       <div className="ml-auto">
-        <UserButton />
+        {/* Remove UserButton component */}
       </div>
     </nav>
   );
--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -2,7 +2,7 @@ import { Banner } from "./banner";
 import { ProjectsSection } from "./projects-section";
 import { TemplatesSection } from "./templates-section";
 
-export default function Home() {
+export default function Dashboard() {
   return (
     <div className="flex flex-col space-y-6 max-w-screen-xl mx-auto pb-10">
       <Banner />
